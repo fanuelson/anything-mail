c97a7fd4fb0d33acc75e84a1281f722bd9e3e1c9
fanuel nunes <fanuelson@gmail.com>
2016-08-17 00:45:46 -0300

    Commons Lang3 Reflect.

--- src/main/java/br/com/any/template/AbstractVelocityTemplate.java
+++ src/main/java/br/com/any/template/AbstractVelocityTemplate.java
@@ -5,18 +5,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.lang3.reflect.FieldUtils;
+
 import br.com.any.annotation.VelocityParameter;
-import br.com.any.utils.ReflectionUtils;
 
 public abstract class AbstractVelocityTemplate {
 
 	public Map<String, Object> getParametros() {
 		Map<String, Object> data = new HashMap<String, Object>();
-		List<Field> fields = ReflectionUtils.recuperarCamposAnotadosCom(getClass(), VelocityParameter.class);
-		for (Field field : fields) {
-			Object valor = ReflectionUtils.recuperarValorCampo(this, field.getName());
-			String parameterName = getParameterName(field);
-			data.put(parameterName, valor);
+		try{
+			
+			List<Field> fields = FieldUtils.getFieldsListWithAnnotation(getClass(), VelocityParameter.class);
+			for (Field field : fields) {
+				Object valor = FieldUtils.readField(this, field.getName(), true);
+				String parameterName = getParameterName(field);
+				data.put(parameterName, valor);
+			}
+		} catch (IllegalArgumentException e){
+			throw new RuntimeException("REFLECT ERROR ",e);
+		} catch (IllegalAccessException e) {
+			throw new RuntimeException("REFLECT ERROR ",e);
 		}
 		return data;
 	}
