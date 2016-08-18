e0aa36263f9d62928ec67a6083d57fceaefbf722
ffnunes <fanuelson@gmail.com>
2016-08-18 10:52:50 -0300

    Implementação suporta ABSOLUTE PATH and PROJECT RESOURCE PATH.

--- src/main/java/br/com/any/service/VelocityGeneratorService.java
+++ src/main/java/br/com/any/service/VelocityGeneratorService.java
@@ -7,6 +7,7 @@ import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.VelocityEngine;
 import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 import br.com.any.annotation.VelocityTemplate;
 import br.com.any.template.AbstractVelocityTemplate;
@@ -14,6 +15,8 @@ import br.com.any.template.AbstractVelocityTemplate;
 public class VelocityGeneratorService implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	private static final String RESOURCE_PATH = "/src/main/resources/";
 
 	private VelocityEngine ve;
 
@@ -22,13 +25,20 @@ public class VelocityGeneratorService implements Serializable {
 	}
 
 	public String generateString(AbstractVelocityTemplate velocityTemplate) {
-
-		this.ve.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, velocityTemplate.getTemplateDirectory());
+		String templateDirectory = velocityTemplate.getTemplateDirectory();
+		if(templateDirectory.contains(RESOURCE_PATH)){
+			ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+			ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+			templateDirectory = templateDirectory.replace(RESOURCE_PATH, "");
+		}else{
+			this.ve.setProperty(RuntimeConstants.FILE_RESOURCE_LOADER_PATH, velocityTemplate.getTemplateDirectory());
+			templateDirectory = "";
+		}
 		this.ve.init();
 
 		VelocityContext context = new VelocityContext(velocityTemplate.getParametros());
 		String templateFileName = velocityTemplate.getClass().getAnnotation(VelocityTemplate.class).templateFileName();
-		Template t = this.ve.getTemplate(templateFileName);
+		Template t = this.ve.getTemplate(templateDirectory+templateFileName);
 
 		StringWriter writer = new StringWriter();
 
